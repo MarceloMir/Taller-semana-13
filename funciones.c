436904d8f62817877d234422e0024a5947451196
MarceloMir <152456674+MarceloMir@users.noreply.github.com>
2024-07-03 14:59:11 -0500

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -16,36 +16,35 @@ void ingresarCliente(char clientes[][2][40]) {
 }
 
 void imprimirClientes(char clientes[][2][40]) {
-    FILE *archivo = fopen("clientes.txt", "w");
+    FILE *archivo = fopen("clientes.txt", "a");
     if (archivo != NULL) {
         printf("Se abrio correctamente el archivo: clientes.txt\n");
 
-    
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(clientes[i][0], "") != 0) {
-            fprintf(archivo, "%s\t\t%s\n", clientes[i][0], clientes[i][1]);
+        for (int i = 0; i < 5; i++) {
+            if (strcmp(clientes[i][0], "") != 0) {
+                fprintf(archivo, "%s\t\t%s\n", clientes[i][0], clientes[i][1]);
+                printf("%s\t\t%s\n", clientes[i][0], clientes[i][1]);
+            }
         }
         fclose(archivo);
-    }
-    }else{
+    } else {
         printf("No se pudo abrir el archivo clientes.txt\n");
     }
-
-
 }
 
 void listarPeliculas(char peliculas[][4][40]) {
-    FILE *archivo = fopen("Listapeliculas.txt", "w");
+    FILE *archivo = fopen("Listapeliculas.txt", "a");
     if (archivo != NULL) {
-        printf("Se abrió correctamente el arcihvo: Listarpeliculas.txt\n");
-    
-    
-    fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
-    for (int i = 0; i < 10; i++) {
-        fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
-    }
-    fclose(archivo);
-    }else{
+        printf("Se abrió correctamente el archivo: Listarpeliculas.txt\n");
+        fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
+        printf("numero\t\tPelicula\t\tHora\t\tGenero\n");
+
+        for (int i = 0; i < 10; i++) {
+            fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+            printf("\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+        }
+        fclose(archivo);
+    } else {
         printf("No se pudo abrir el archivo Listarpeliculas.txt");
     }
 }
@@ -54,19 +53,20 @@ void buscarporNombre(char peliculas[][4][40]) {
     char nPelicula[40];
     printf("Ingrese el nombre de la pelicula que desee buscar: \n");
     scanf("%s", nPelicula);
-    FILE *archivo = fopen("busquedaDepelicula.txt", "w");
+    FILE *archivo = fopen("busquedaDepelicula.txt", "a");
     if (archivo != NULL) {
-        printf("Abierto correctamente: busquedaDepelicula.txt");
-    
-    
-    fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
-    for (int i = 0; i < 10; i++) {
-        if (strcmp(peliculas[i][1], nPelicula) == 0) {
-            fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+        printf("Abierto correctamente: busquedaDepelicula.txt\n");
+        fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
+        printf("numero\t\tPelicula\t\tHora\t\tGenero\n");
+
+        for (int i = 0; i < 10; i++) {
+            if (strcmp(peliculas[i][1], nPelicula) == 0) {
+                fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+                printf("\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+            }
         }
-    }
-    fclose(archivo);
-    }else{
+        fclose(archivo);
+    } else {
         printf("No se pudo abrir el archivo busquedaDepelicula.txt");
     }
 }
@@ -75,19 +75,20 @@ void buscarporGenero(char peliculas[][4][40]) {
     char Gen[40];
     printf("Ingrese el genero de la pelicula que desea buscar: ");
     scanf("%s", Gen);
-    FILE *archivo = fopen("busquedaPorgenero.txt", "w");
+    FILE *archivo = fopen("busquedaPorgenero.txt", "a");
     if (archivo != NULL) {
-        printf("Abierto Correctamente: BuscarPorgenero.txt");
-    
-    
-    fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
-    for (int i = 0; i < 10; i++) {
-        if (strcmp(peliculas[i][3], Gen) == 0) {
-            fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+        printf("Abierto Correctamente: BuscarPorgenero.txt\n");
+        fprintf(archivo, "numero\t\tPelicula\t\tHora\t\tGenero\n");
+        printf("numero\t\tPelicula\t\tHora\t\tGenero\n");
+
+        for (int i = 0; i < 10; i++) {
+            if (strcmp(peliculas[i][3], Gen) == 0) {
+                fprintf(archivo, "\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+                printf("\n%s\t\t%s\t\t%s\t\t%s\n", peliculas[i][0], peliculas[i][1], peliculas[i][2], peliculas[i][3]);
+            }
         }
-    }
-    fclose(archivo);
-    }else{
+        fclose(archivo);
+    } else {
         printf("No se pudo abrir el archivo BuscarPorgenero.txt");
     }
 }
@@ -135,58 +136,65 @@ void comprarTicket(char peliculas[][4][40], double precio[], char clientes[][2][
         printf("\n\tIngrese al menos un boleto\n");
     }
     double PrecioTotal = cantGeneral * precioGeneral + cantVermouth * precioVermouth + cantTerceraEdad * precioTerceraEdad;
-    FILE *archivo = fopen("comprar ticket.txt", "w");
+    FILE *archivo = fopen("comprar_ticket.txt", "a");
     if (archivo != NULL) {
-        printf("Se abrio el archivo: comprar ticket.txt");
-        
-    
-    fprintf(archivo, "\nESTE ES EL RESUMEN DE SU COMPRA...\n");
-    fprintf(archivo, "Cliente: %s\n", nombre);
-    fprintf(archivo, "Pelicula: %s\n", peliculas[numPelicula - 1][1]);
-    fprintf(archivo, "Cantidad de Entradas Generales: %d\n", cantGeneral);
-    fprintf(archivo, "Cantidad de Entradas Vermouth: %d\n", cantVermouth);
-    fprintf(archivo, "Cantidad de Entradas Tercera Edad: %d\n", cantTerceraEdad);
-    fprintf(archivo, "El precio TOTAL es: %.2f\n", PrecioTotal);
-   
-    int cantidadTotalBoletos = cantGeneral + cantVermouth + cantTerceraEdad;
-    int confirmacion;
-    printf("\nDesea confirmar la compra? 1.si / 2.no ");
-    scanf("%d", &confirmacion);
-    if (confirmacion == 1) {
-        reserva[numPelicula - 1][indexNombre] = cantidadTotalBoletos;
-        fprintf(archivo, "\n\tCOMPRA CONFIRMADA\n");
+        printf("Se abrió el archivo: comprar_ticket.txt\n");
+        fprintf(archivo, "\nESTE ES EL RESUMEN DE SU COMPRA...\n");
+        fprintf(archivo, "Cliente: %s\n", nombre);
+        fprintf(archivo, "Pelicula: %s\n", peliculas[numPelicula - 1][1]);
+        fprintf(archivo, "Cantidad de Entradas Generales: %d\n", cantGeneral);
+        fprintf(archivo, "Cantidad de Entradas Vermouth: %d\n", cantVermouth);
+        fprintf(archivo, "Cantidad de Entradas Tercera Edad: %d\n", cantTerceraEdad);
+        fprintf(archivo, "El precio TOTAL es: %.2f\n", PrecioTotal);
+        printf("\nESTE ES EL RESUMEN DE SU COMPRA...\n");
+        printf("Cliente: %s\n", nombre);
+        printf("Pelicula: %s\n", peliculas[numPelicula - 1][1]);
+        printf("Cantidad de Entradas Generales: %d\n", cantGeneral);
+        printf("Cantidad de Entradas Vermouth: %d\n", cantVermouth);
+        printf("Cantidad de Entradas Tercera Edad: %d\n", cantTerceraEdad);
+        printf("El precio TOTAL es: %.2f\n", PrecioTotal);
+
+        int cantidadTotalBoletos = cantGeneral + cantVermouth + cantTerceraEdad;
+        int confirmacion;
+        printf("\nDesea confirmar la compra? 1.si / 2.no ");
+        scanf("%d", &confirmacion);
+        if (confirmacion == 1) {
+            reserva[numPelicula - 1][indexNombre] = cantidadTotalBoletos;
+            fprintf(archivo, "\n\tCOMPRA CONFIRMADA\n");
+            printf("\n\tCOMPRA CONFIRMADA\n");
+        } else {
+            fprintf(archivo, "\n\tLA COMPRA HA SIDO CANCELADA\n");
+            printf("\n\tLA COMPRA HA SIDO CANCELADA\n");
+        }
+        fclose(archivo);
     } else {
-        fprintf(archivo, "\n\tLA COMPRA HA SIDO CANCELADA\n");
-    }
-    fclose(archivo);
-    }else{
-        printf("No se pudo abrir el archivo comprar ticket.txt");
+        printf("No se pudo abrir el archivo comprar_ticket.txt");
     }
 }
 
 void verCompras(char peliculas[][4][40], double precio[3], char clientes[][2][40], int reserva[][4]) {
-    FILE *archivo = fopen("Ver compras.txt", "w");
+    FILE *archivo = fopen("ver_compras.txt", "a");
     if (archivo != NULL) {
-        printf("Se abrio correctamente: Ver compras.txt");
-   
-    
-    fprintf(archivo, "Estas son sus compras realizadas\n");
-    int hayCompras = 0;
+        printf("Se abrió correctamente: ver_compras.txt\n");
+        fprintf(archivo, "Estas son sus compras realizadas\n");
+        printf("Estas son sus compras realizadas\n");
+        int hayCompras = 0;
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (reserva[i][j] != -1 && strcmp(clientes[j][0], "") != 0) {
-                fprintf(archivo, "Cliente: %s, Pelicula: %s, Cantidad de boletos: %d\n", clientes[j][0], peliculas[i][1], reserva[i][j]);
-                hayCompras = 1;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (reserva[i][j] != -1 && strcmp(clientes[j][0], "") != 0) {
+                    fprintf(archivo, "Cliente: %s, Pelicula: %s, Cantidad de boletos: %d\n", clientes[j][0], peliculas[i][1], reserva[i][j]);
+                    printf("Cliente: %s, Pelicula: %s, Cantidad de boletos: %d\n", clientes[j][0], peliculas[i][1], reserva[i][j]);
+                    hayCompras = 1;
+                }
             }
         }
-    }
-    if (!hayCompras) {
-        fprintf(archivo, "No se han realizado compras.\n");
-    }
-    fclose(archivo);
-    }else{
-        printf("No se pudo abrir el archivo Ver compras.txt");
+        if (!hayCompras) {
+            fprintf(archivo, "No se han realizado compras.\n");
+            printf("No se han realizado compras.\n");
+        }
+        fclose(archivo);
+    } else {
+        printf("No se pudo abrir el archivo ver_compras.txt");
     }
 }
-
